a1b1a77cbb04be8c7c30eade2820bfd3d71175d4
Romeo Van Snick <romeovs@gmail.com>
2016-04-06 11:43:13 +0200

    add all cli arguments

--- gulp.plugin.zsh
+++ gulp.plugin.zsh
@@ -13,7 +13,15 @@ function _gulp () {
   compls=(${=tasks})
 
   _arguments \
-    '-v[show version]' \
+    {-v,--version}'[display the global and local gulp versions]' \
+    '--require[require a module before running the gulpfile]' \
+    {-T,--tasks}'[display the task dependency tree for the loaded gulpfile]' \
+    '--gulpfile[manually set path of gulpfile]:file:_files' \
+    '--cwd[manually set the CWD]:directory:_directories' \
+    '--tasks-simple[display a plaintext list of tasks for the loaded gulpfile]' \
+    '--color[force gulp and gulp plugins to display colors]' \
+    '--no-color[force gulp and gulp plugins to not display colors]' \
+    '--silent[disable all gulp logging]' \
     '*::task:( $compls )'
 }
 
